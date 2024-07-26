b02c738d99a05098284200459d8c457888c94c4e
rubsnd <rubsnd@gmail.com>
2024-07-26 09:19:50 +0000

    C1

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+
+
+resource "aws_vpc" "dep1" {
+    cidr_block = "192.168.0.0/16"
+    tags = {
+      "Name" = "VPC1"
+      
+    }
+    depends_on = [ aws_vpc.dep3 ]
+  
+}
